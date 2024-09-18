1d138599ece1b6855a371cb169f01a7cfed50111
Okasha-Rehman <okasharehman6000@gmail.com>
2024-09-18 15:00:47 +0500

    Added Message Submission

--- backend/app.py
+++ backend/app.py
@@ -1,47 +1,6 @@
-'''from flask import Flask, request, jsonify
-import psycopg2
-
-app = Flask(__name__)
-
-# Connect to PostgreSQL database
-def get_db_connection():
-    conn = psycopg2.connect(
-        host="db", 
-        database="contacts_db", 
-        user="postgres", 
-        password="password"
-    )
-    return conn
-
-# Route for root
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-
-# Route for form submission
-@app.route('/submit_contact', methods=['POST'])
-def submit_contact():
-    name = request.form['name']
-    email = request.form['email']
-
-    conn = get_db_connection()
-    cur = conn.cursor()
-    
-    cur.execute('INSERT INTO contacts (name, email) VALUES (%s, %s)', (name, email))
-    conn.commit()
-
-    cur.close()
-    conn.close()
-
-    return jsonify({"message": "Contact submitted!"}), 200
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
-'''   
-    
 from flask import Flask, request, jsonify, render_template
 import psycopg2
+
 app = Flask(__name__)
 
 # Connect to PostgreSQL database
@@ -64,13 +23,14 @@ def home():
 def submit_contact():
     name = request.form['name']
     email = request.form['email']
+    message = request.form['message']  # Added this line to get the message
 
     conn = get_db_connection()
     cur = conn.cursor()
     
-    cur.execute('INSERT INTO contacts (name, email) VALUES (%s, %s)', (name, email))
+    # Updated SQL query to include the message
+    cur.execute('INSERT INTO contacts (name, email, message) VALUES (%s, %s, %s)', (name, email, message))
     conn.commit()
-
     cur.close()
     conn.close()
 
--- db/init.sql
+++ db/init.sql
@@ -1,6 +1,7 @@
+-- Create the table if it doesn't exist
 CREATE TABLE IF NOT EXISTS contacts (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100),
-    email VARCHAR(100)
+    email VARCHAR(100),
+    message TEXT
 );
-
